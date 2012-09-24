8a43b118edf7dd1f37f05b60d401907c5ed8629a
Samuel Ashelford <sam@Samuel-Ashelfords-iMac.local>
2012-09-24 21:13:35 +1000

    basic algorith- partially workable

---
+++ antiquity.py
@@ -0,0 +1,26 @@
+
+import math
+
+
+
+
+def julian_day(year, month, day):
+    """
+    see http://www.hermetic.ch/cal_stud/jdn.htm#comp
+    http://www.merlyn.demon.co.uk/daycount.htm#GDDC
+    http://www.tondering.dk/claus/cal/julperiod.php#formula
+    http://articles.adsabs.harvard.edu/cgi-bin/nph-iarticle_query?bibcode=1983IAPPP..13...16F&db_key=AST&page_ind=0&data_type=GIF&type=SCREEN_VIEW&classic=YES
+    """
+
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    a = (14-month)/12
+    y = year + 4800 - a
+    print a,"a"
+    m = month + 12*a - 3
+    print m,"m"
+    print y,"y"
+    return day+(153*m+2)/5+365*y+y/4-y/100+y/400-32045
+
+    
