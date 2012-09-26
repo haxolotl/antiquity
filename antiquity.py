e4ba2ea8c2499180c07bb0861ff8de142aa76ea5
Haxolotl <samuel.ashelford@gmail.com>
2012-09-26 17:06:52 +1000

    'refactoring'

--- antiquity.py
+++ antiquity.py
@@ -12,16 +12,24 @@ class HistoricalDate:
         http://www.tondering.dk/claus/cal/julperiod.php#formula
         http://articles.adsabs.harvard.edu/cgi-bin/nph-iarticle_query?bibcode=1983IAPPP..13...16F&db_key=AST&page_ind=0&data_type=GIF&type=SCREEN_VIEW&classic=YES
         """
+        
+        self.year = int(year)
+        self.month = int(month)
+        self.day = int(day)
+        self.julian_day = self._julian_day()    
+        self.century = self._century()
 
-        year = int(year)
-        month = int(month)
-        day = int(day)
-        a = (14-month)/12
-        y = year + 4800 - a
-#        print a,"a"
-        m = month + 12*a - 3
-#        print m,"m"
-#        print y,"y"
-        self.julian_day = day+(153*m+2)/5+365*y+y/4-y/100+y/400-32045
-
-    
+    def _century(self):
+        if self.year > 0:
+            return (self.year-1)/100+1
+        elif self.year < 0:
+            return (self.year-1)/100-1
+        else:
+            print "you're stupid"    
+        
+    def _julian_day(self):
+        a = (14-self.month)/12
+        y = self.year + 4800 - a
+        m = self.month + 12*a - 3
+        return self.day+(153*m+2)/5+365*y+y/4-y/100+y/400-32045
+        
