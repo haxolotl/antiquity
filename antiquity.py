12ec35236f10e66cf7bb9987124c9198ecfdf87a
Haxolotl <samuel.ashelford@gmail.com>
2012-09-29 11:36:07 +1000

    PJDay working despite out by 1 error of wikipedia's algorithm

--- antiquity.py
+++ antiquity.py
@@ -3,7 +3,6 @@ import math
 from datetime import date
 from exceptions import ValueError
 
-
 class HistoricalDate:
 
     def __init__(self, year, month, day):
@@ -38,4 +37,41 @@ class HistoricalDate:
         y = self._year + 4800 - a
         m = self.month + 12*a - 3
         return self.day+(153*m+2)/5+365*y+y/4-y/100+y/400-32045
+       
+class PJDay:
+
         
+	def __init__(self,day_number):
+		self.number = int(math.floor(day_number))
+# 		for now we'll just use integers. fractional days can come later.
+
+	@property
+	def date(self):
+		j = self.number + 32045
+		g = j/146097
+		dg = j % 146097
+		c = (dg/36524 + 1 ) * 3/4 
+		dc = dg - c * 36524
+		b = dc/1461
+		db = dc % 1461
+		a = (db/365 + 1 ) * 3/4
+		da = db - a * 365
+		y = g * 400 + c * 100 + b * 4 + a 
+		m = (da * 5 +308)/153 - 2 
+		d = da - (m + 4) * 153/5 + 122
+		Y = y - 4800 + (m + 2)/12
+		M = (m + 2) % 12 + 1 
+		D = d + 1
+		return (Y, M, D)
+	
+	@property
+	def year(self):
+		return self.date[0]
+
+	@property		
+	def month(self):
+		return self.date[1]
+		
+	@property
+	def day(self):
+		return self.date[2]
