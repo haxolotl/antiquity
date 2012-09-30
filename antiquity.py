3bd81171e6169660bf0f67e6d4db6b4274313815
Thomas Ashelford <thomas@ether.com.au>
2012-09-30 22:12:51 +1000

    Yay - we can convert a date into the same date using really complex maths

--- antiquity.py
+++ antiquity.py
@@ -2,46 +2,11 @@
 import math
 from datetime import date
 from exceptions import ValueError
-
-class HistoricalDate:
-
-    def __init__(self, year, month, day):
-        """
-        see http://www.hermetic.ch/cal_stud/jdn.htm#comp
-        http://www.merlyn.demon.co.uk/daycount.htm#GDDC
-        http://www.tondering.dk/claus/cal/julperiod.php#formula
-        http://articles.adsabs.harvard.edu/cgi-bin/nph-iarticle_query?bibcode=1983IAPPP..13...16F&db_key=AST&page_ind=0&data_type=GIF&type=SCREEN_VIEW&classic=YES
-        """
-        self.year = int(year)
-        # _year is for internal calculations only, not for humans
-        # 1BC = 0, 2BC = -1 etc.
-        if int(year) > 0:
-            self._year = int(year)
-        elif int(year) < 0:
-            self._year = int(year) + 1
-        else:
-            raise ValueError("http://lmgtfy.com/?q=year+0")
-        self.month = int(month)
-        self.day = int(day)
-        self.julian_day = self._julian_day()    
-        self.century = self._century()
-
-    def _century(self):
-        if self._year > 0:
-            return (self._year-1)/100+1
-        else:
-            return (self._year-1)/100 # integer division of negative number is weird!
-        
-    def _julian_day(self):
-        a = (14-self.month)/12
-        y = self._year + 4800 - a
-        m = self.month + 12*a - 3
-        return self.day+(153*m+2)/5+365*y+y/4-y/100+y/400-32045
        
 class PJDay:
 
         
-	def __init__(self,day_number):
+	def __init__(self, day_number):
 		self.number = int(math.floor(day_number))
 # 		for now we'll just use integers. fractional days can come later.
 
@@ -62,6 +27,8 @@ class PJDay:
 		Y = y - 4800 + (m + 2)/12
 		M = (m + 2) % 12 + 1 
 		D = d + 1
+		if Y < 1:
+		  Y = Y - 1
 		return (Y, M, D)
 	
 	@property
@@ -75,3 +42,21 @@ class PJDay:
 	@property
 	def day(self):
 		return self.date[2]
+		
+class PGDate(PJDay):
+    def __init__(self, year, month, day):
+        year = int(year)
+        if year < 0:
+            year = year + 1
+        elif year == 0:
+            raise ValueError("http://lmgtfy.com/?q=year+0")
+        month = int(month)
+        if month > 12 or month < 1:
+            raise ValueError("Month must not be less than 1 or greater than 12")
+        day = int(day) # TO DO - validate input
+        a = (14-month)/12
+        y = year + 4800 - a
+        m = month + 12*a - 3
+        self.number = day+(153*m+2)/5+365*y+y/4-y/100+y/400-32046
+    
+    
